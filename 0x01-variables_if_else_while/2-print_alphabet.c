9eb21c03b0cfd080ca41a65a7703328a826f724d
Benard Githu <githubenard16@gmail.com>
2023-03-17 18:02:11 +0300

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <ctype.h>
+/**
+*main - Program to print alphabet letter followed by new line
+*
+*Return: return 0
+*/
+int main(void)
+{
+int alpha;
+for(alpha='a'; alpha<='z'; alpha++)
+{
+	alpha = tolower(alpha);
+	putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
