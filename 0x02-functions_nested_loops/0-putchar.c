e85da974fadad8cc4cc26d7875d2f671460aba05
Benard Githu <githubenard16@gmail.com>
2023-03-21 14:14:40 +0300

    add README.md and 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*main - Prints all single digits of base 16
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+	char pt[9];
+
+	strcpy(pt, "_putchar");
+	puts(pt);
+	puts("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+tasks on C functions and nested loops
