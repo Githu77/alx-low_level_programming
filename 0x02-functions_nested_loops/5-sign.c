7b28fb644136f4f379742ed04002671cdf0ad71a
Benard Githu <githubenard16@gmail.com>
2023-03-21 17:36:40 +0300

    add 5-sign.c and main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*print_sign - prints the sign of a number
+*@n: the character being assessed
+*Return: returns 1, 0 or -1
+*/
+int print_sign(int n)
+{
+	char great, less, equal;
+
+	great = '+';
+
+	less = '-';
+
+	equal = '0';
+
+	if (n > 0)
+	{
+		_putchar(great);
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(equal);
+		return (0);
+	} else
+	{
+		_putchar(less);
+		return (-1);
+	}
+}
