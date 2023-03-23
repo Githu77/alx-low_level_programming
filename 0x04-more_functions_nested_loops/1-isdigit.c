e205e8f96277b89bde6352e7f251db5316921630
Benard Githu <githubenard16@gmail.com>
2023-03-23 12:46:54 +0300

    fix 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,14 +5,16 @@
 *Return: returns 1 if c is digit or 0 if not
 */
 int _isdigit(int c)
-{	
-	if ((c >= 48) && (c <= 57))
+{
+	for (c = 0; c <= 9; c++)
 	{
-		return (1);
-	
-	}
-	else
-	{
-		return (0);
+		if ((c >= 'a' || c <= 'z') && (c >= 'A' || c <= 'Z'))
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
 	}
 }
