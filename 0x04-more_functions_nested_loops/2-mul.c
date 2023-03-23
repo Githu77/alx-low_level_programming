c965198003c1168784a8d006d74721b61560dc3f
Benard Githu <githubenard16@gmail.com>
2023-03-23 14:35:48 +0300

    add 1-isdigit.c  3-print_numbers.c  _putchar.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*mul - multiplies two integers
+*@a: first integer
+*@b: secod integer
+*Return: returns multiplicaton of  integers
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
