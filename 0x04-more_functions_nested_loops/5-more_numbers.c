18f68de9cb6645cd3774ad8677aedc55843ed5e0
Benard Githu <githubenard16@gmail.com>
2023-03-23 17:31:52 +0300

    add 5-more_numbers.c and main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*more_numbers - prints numbers 0 -14 ten times
+*
+*/
+void more_numbers(void)
+{
+	int nums;
+
+	for (nums = 0; nums = 10; nums++)
+	{
+		int nums1;
+
+		for (nums1 = 0; nums <= 14; nums++)
+		{
+			if (nums1 > 9)
+			{
+				_putchar((nums1 / 10) + '0');
+			}
+			else
+			{
+				_putchar((nums1 % 10) + '0');
+			}
+			_putchar(10);
+		}
+	}
+}
