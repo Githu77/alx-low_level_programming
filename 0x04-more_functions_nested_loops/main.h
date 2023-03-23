5fade118f1536e46991940b1e3263c4fb71a117f
Benard Githu <githubenard16@gmail.com>
2023-03-23 17:15:03 +0300

    add main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-#endif /* MAIN_H */
+void print_most_numbers(void);
