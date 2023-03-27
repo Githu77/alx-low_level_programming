8d63d26fd485b59127bd4f9bc06a5eff3d5598e9
Benard Githu <githubenard16@gmail.com>
2023-03-27 15:22:17 +0300

    add main.h and 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+*rev_string - reverses a string
+*@s: string in question
+*Return: returns nothing
+*
+*/
+void rev_string(char *s)
+{
+	int i = 0,
+
+	len;
+
+	len = _strlen(s) - 1;
+
+	while (len > i)
+	{
+		swap_char(s + len, s + i);
+		i++;
+		len--;
+	}
+}
+
+/**
+*_strlen - counts length of string
+*@s: string in question
+*Return: returns length
+*
+*/
+int _strlen(char *s)
+{
+	int cnt, len;
+
+	len = 0;
+
+	for (cnt = 0; s[cnt] != '\0'; cnt++)
+	len++;
+
+	return (len);
+}
+
+/**
+*swap_int - swaps the content of the pointers
+*@a: first pointer
+*@b: second pointer
+*Return : returns nothing
+*
+*/
+void swap_char(char *a, char *b)
+{
+	char ch = *a;
+	*a = *b;
+	*b = ch;
+}
