9030b9519437e0af7677baa3b20286309d993d9b
Benard Githu <githubenard16@gmail.com>
2023-03-27 13:35:17 +0300

    adding main.h README.md 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*reset_to_98 - resets pointer value to 98
+*@n: the pointer being updated
+*Return : returns nothing
+*/
+void reset_to_98(int *n)
+{
+	int o;
+	o = *n;
+	o = 98;
+}
