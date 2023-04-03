661e9a1463d42ef4f7c5c2aa00cd8e13b362840b
Benard Githu <githubenard16@gmail.com>
2023-04-03 18:16:27 +0300

    adding main.h and 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+*_strspn - gets length of prefix substring
+*@s: the section in question
+*@accept: the source
+*Return: returns bytes number
+*
+*
+*
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+
+	int z = 0;
+
+	while (s[i] != '\0')
+	{
+		int x = 0;
+
+		while (accept[x] != '\0')
+		{
+			if (s[i] == accept[x])
+			{
+				z++;
+				break;
+			}
+			x++;
+			if (s[i] != accept[x])
+			{
+				return (z);
+			}
+		}
+		i++;
+	}
+	return (z);
+}
