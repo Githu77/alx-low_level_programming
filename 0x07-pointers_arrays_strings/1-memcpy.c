003ab889e2ffc4987e09aca2f5ab799ff9b302ad
Benard Githu <githubenard16@gmail.com>
2023-04-03 16:34:28 +0300

    add main.h and 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_memcpy - copies from a source and pastes in a destination
+*@dest: the destination
+*@src: the source
+*@n: bytes to copy
+*Return: returns the final string
+*
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
