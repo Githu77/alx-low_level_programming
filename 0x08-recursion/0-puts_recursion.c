9c65cf935d46826279f34d39317f6d5cf4b5cec0
Benard <githubenard16@gmail.com>
2023-04-05 13:25:17 +0300

    adding main.h and 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_puts_recursion - prints string using recursion
+*@s: the string
+*Return: returns nothing
+*
+*
+*
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(*s + 1);
+}
