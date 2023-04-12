6f7acfe5e387f950cb71763589d399c11bc908e2
Benard <githubenard16@gmail.com>
2023-04-12 18:59:23 +0300

    adding 100-argstostr.c and main.h

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+*argstostr - concatenates strings passed as arguments
+*@ac: no of arguments
+*@av: arguments passed
+*Return: returns either NULL or pointer to string
+*
+*
+*
+*/
+char *argstostr(int ac, char **av)
+{
+	int len, t_len = 0, cpy = 0;
+	char *arg, *nstr;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (len = 0; len < ac; len++)
+	{
+		arg = av[len];
+		while (*arg != '\0')
+		{
+			t_len++;
+			arg++;
+		}
+		t_len = t_len + 1;
+	}
+	nstr = (char *) malloc(sizeof(char) * t_len);
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (len = 0; len < ac; len++)
+	{
+		char *arg = av[len];
+
+		while (*arg != '\0')
+		{
+			nstr[cpy] = *arg;
+			arg++;
+			cpy++;
+		}
+		nstr[cpy] = '\n';
+		cpy++;
+	}
+	nstr[cpy] = '\0';
+
+	return (nstr);
+}
