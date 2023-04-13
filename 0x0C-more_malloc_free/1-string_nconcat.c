3755cb1765b58be91fdc1c45600205b54e102a8a
Benard <githubenard16@gmail.com>
2023-04-13 15:53:48 +0300

    fix 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,10 +5,7 @@
 *@s2: string two
 *@n: number of chars to coppy from s2
 *Return: returns NULL or pointer to new string
-*
-*
 */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int s1len = 0, s2len = 0, i, j;
@@ -16,14 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 != NULL)
 	{
-		while (s1[s1len] != '\0')
-		{
-			s1len++;
-		}
-	}
-	else
-	{
-		s1 = "";
 		while (s1[s1len] != '\0')
 		{
 			s1len++;
@@ -37,33 +26,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			s2len++;
 		}
 	}
-	else
-	{
-		s2 = "";
-		while (s2[s2len] != '\0')
-		{
-			s2len++;
-		}
-	}
 
 	nstr = malloc(sizeof(char) * (s1len + n + 1));
-
 	if (nstr == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < s1len; i++)
 	{
 		nstr[i] = s1[i];
 	}
-
 	for (j = 0; j < n && j < s2len; j++)
 	{
 		nstr[i + j] = s2[j];
 	}
-
 	nstr[i + j] = '\0';
-
 	return (nstr);
 }
