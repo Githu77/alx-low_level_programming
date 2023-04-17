88fd50c37a6d981d32e6eebc50c3382cdb638ab5
Benard <githubenard16@gmail.com>
2023-04-17 15:42:39 +0300

    adding dog.h and 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+*init_dog - initialize type dogs variables
+*@d: dog
+*@name: dog's name
+*@age: dog's age
+*@owner: dog's owner
+*
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d = NULL)
+	{
+		return;
+	}
+	else
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
