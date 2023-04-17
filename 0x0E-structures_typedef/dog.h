ecef78005dba9c097231396535e57f6672f36aeb
Benard <githubenard16@gmail.com>
2023-04-17 16:12:31 +0300

    add dog.h and 2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdlib.h>
+#include <stdio.h>
 /**
 *struct dog - datatype for dog
 *@name: dog's name
@@ -19,5 +20,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
