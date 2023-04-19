2acedbd6f61733b19b543be24a809646aeecaaf6
Benard <githubenard16@gmail.com>
2023-04-20 01:18:02 +0300

    fix 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,72 @@
 #include "3-calc.h"
-/**
-*op_add - adds two numbers
-*@a: 1st no
-*@b: 2nd no
-*Return: (a+b)
-*
-*
-*
-*/
 
+/**
+ * op_add - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
-*op_mul - multiply two numbers
-*@a: 1st no
-*@b: 2nd no
-*Return: (a*b)
-*
-*
-*
-*
-*/
-int op_mul(int a, int b)
-{
-return (a * b);
-}
-
-/**
-*op_div - divides two numbers
-*@a: 1st no
-*@b: 2nd no
-*Return: (a/b)
-*
-*
-*
-*
-*/
-int op_div(int a, int b)
-{
-return (a / b);
-}
-
-/**
-*op_sub - subtracts two numbers
-*@a: 1st no
-*@b: 2nd no
-*Return: (a-b)
-*
-*
-*
-*
-*/
+ * op_sub - Subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 
 /**
-*op_mod - modulo of number
-*@a: 1st no
-*@b: 2nd no
-*Return: (a%b)
-*
-*
-*
-*
-*/
+ * op_mul - Multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of division of a by b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Computes modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of division of a by b
+ */
 int op_mod(int a, int b)
 {
-return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
+
