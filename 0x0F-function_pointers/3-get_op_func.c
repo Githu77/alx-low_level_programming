dc44c36af5e343dafae32a0b5e14e490aaaac0bf
Benard <githubenard16@gmail.com>
2023-04-19 22:07:10 +0300

    fix 3-calc.h 3-main.c  3-get_op_func.c  3-op_functions.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	for (int i = 0; ops[i].op != NULL; i++)
+	for (i = 0; ops[i].op != NULL; i++)
 	{
 		if (*(ops[i].op) == *s)
 		{
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,9 +1,10 @@
 #include  "3-calc.h"
+#include <stdio.h>
 /**
 *main - does add, subtract, division and multiplication
 *@argc: no of commands
 *@argv: array of commands
-*
+*Return: either 98 or 0
 *
 *
 *
@@ -11,58 +12,61 @@
 */
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return 98;
-    }
+	int num1, num2, result;
+	char op;
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[3]);
-    char op = *argv[2];
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    int result = calculate(num1, num2, op);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = *argv[2];
 
-    printf("%d\n", result);
-    return 0;
+	result = calculate(num1, num2, op);
+
+	printf("%d\n", result);
+	return (0);
 }
 
 /**
-*calculate: - handles different operators
+*calculate - handles different operators
 *@num1: first number
 *@num2: second number
-*
-*
+*@op: option selected
+*Return: result or error
 *
 *
 *
 */
 int calculate(int num1, int num2, char op)
 {
-    switch (op)
-    {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '*':
-            return num1 * num2;
-        case '/':
-            if (num2 == 0)
-            {
-                printf("Error\n");
-                exit(100);
-            }
-            return num1 / num2;
-        case '%':
-            if (num2 == 0)
-            {
-                printf("Error\n");
-                exit(100);
-            }
-            return num1 % num2;
-        default:
-            printf("Error\n");
-            exit(99);
-    }
+	switch (op)
+	{
+		case '+':
+			return (num1 + num2);
+		case '-':
+			return (num1 - num2);
+		case '*':
+			return (num1 * num2);
+		case '/':
+		if (num2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		return (num1 / num2);
+		case '%':
+		if (num2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		return (num1 % num2);
+		default:
+		    printf("Error\n");
+		    exit(99);
+	}
 }
