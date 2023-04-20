73fa9959b9f35b0069cd26c4052d3388fa4e767e
Benard <githubenard16@gmail.com>
2023-04-20 19:21:24 +0300

    fix variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,7 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_formatted(const char * const format, va_list args, const char *separator)
+void print_formatted(const char * const format, va_list args,
+			const char *separator)
 
 #endif
