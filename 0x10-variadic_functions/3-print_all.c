297c7757653461745501fa1801c1fbb048582f1f
Benard <githubenard16@gmail.com>
2023-04-21 21:00:22 +0300

    fix 3-print_all.c variadic_functions.h

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,44 +1,87 @@
-#include "variadic_functions.h"
-#include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_all - prints all types
- * @format: type of argument
+ * print_char - Prints a char
+ * @args: contain the char to print
  */
+void print_char(va_list *args)
+{
+	char c = va_arg(*args, int);
+
+	printf("%c", c);
+}
+
+/**
+ * print_int - Prints an int
+ * @args: contain the int to print
+ */
+void print_int(va_list *args)
+{
+	int n = va_arg(*args, int);
+
+	printf("%d", n);
+}
+
+/**
+ * print_float - Prints a float
+ * @args: contain the float to print
+ */
+void print_float(va_list *args)
+{
+	double f = va_arg(*args, double);
+
+	printf("%f", f);
+}
+
+/**
+ * print_string - Prints a string
+ * @args: contain the string to print
+ */
+void print_string(va_list *args)
+{
+	char *s = va_arg(*args, char *);
+
+	if (s == NULL)
+	s = "(nil)";
+	printf("%s", s);
+}
+
+/**
+*print_all - prints all types
+*@format: type of argument
+*/
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	char *separator = "";
+	int i = 0, j = 0;
+
+	arg_printer_t arg_printers[] = {
+	{'c', &print_char},
+	{'i', &print_int},
+	{'f', &print_float},
+	{'s', &print_string},
+	{0, NULL}
+	};
 
 	va_start(args, format);
 
-	while (format && format[i])
+	while (format[i])
 	{
-		if (format[i] == 'c')
-		{
-			printf("%s%c", separator, va_arg(args, int));
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%d", separator, va_arg(args, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", separator, va_arg(args, double));
-		}
-		else if (format[i] == 's')
-		{
-			char *s = va_arg(args, char *);
+		j = 0;
 
-			printf("%s%s", separator, s ? s : "(nil)");
+		while (arg_printers[j].format)
+		{
+			if (format[i] == arg_printers[j].format)
+			{
+				arg_printers[j].printer(&args);
+				break;
+			}
+			j++;
 		}
-		separator = ", ";
+
 		i++;
 	}
 
-	printf("\n");
 	va_end(args);
 }
-
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,23 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+
+/**
+ * struct arg_printer - struct arg_printer
+ * @specifier: specify character
+ * @printer: pointer to function
+ */
+typedef struct arg_printer
+{
+	char specifier;
+	void (*printer)(va_list *);
+} arg_printer_t;
+
+/* Function prototypes */
+void print_char(va_list *args);
+void print_int(va_list *args);
+void print_float(va_list *args);
+void print_string(va_list *args);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
