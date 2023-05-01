f15e02d631e4a305e204baa2174d590ea363c7ea
Benard <githubenard16@gmail.com>
2023-05-01 09:22:43 +0300

    add README.md 0-print_listint.c lists.h

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+*print_listint - prints all elements of a list
+*@h: points to head of list
+*Return: number of nodes in the list
+*
+*
+*
+*
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t cnt = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		cnt++
+	}
+	return (cnt);
+}
