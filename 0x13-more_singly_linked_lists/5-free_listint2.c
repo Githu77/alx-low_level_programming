48cb74b340c334085d45dd9e7c00b14d536bc1f7
Benard <githubenard16@gmail.com>
2023-05-01 10:35:02 +0300

    add 5-free_listint2.c and fix lists.h

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+*free_listint2 - frees list and sets head to NULL
+*@head: points to pointer that points to head of list
+*
+*
+*
+*
+*
+*
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *first;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	while (*head != NULL)
+	{
+		first = *head;
+		*head = (*head)->next;
+		free(first);
+	}
+}
