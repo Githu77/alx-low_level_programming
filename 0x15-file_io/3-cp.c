b04245a417abe3d75f0f50e26faefaa6384aa6f4
Benard <githubenard16@gmail.com>
2023-05-08 20:53:31 +0300

    add 3-cp.c, update main.h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,58 +15,52 @@
 */
 int main(int argc, char **argv)
 {
-    int fd_from, fd_to, read_count, write_count;
-    char buffer[BUFFER_SIZE];
+	int fd_from, fd_to, read_count, write_count;
+	char buffer[BUFFER_SIZE];
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        return (97);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		print_error("Error: Can't read from file", argv[1], fd_from, -1);
+		return (98);
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		print_error("Error: Can't write to", argv[2], fd_from, fd_to);
+		return (99);
+	}
+	do {
+		read_count = read(fd_from, buffer, BUFFER_SIZE);
+		if (read_count == -1)
+		{
+			print_error("Error: Can't read from file", argv[1], fd_from, fd_to);
+			return (98);
+		}
+		write_count = write(fd_to, buffer, read_count);
+		if (write_count == -1)
+		{
+			print_error("Error: Can't write to", argv[2], fd_from, fd_to);
+			return (99);
+		}
+	} while (read_count > 0);
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        print_error("Error: Can't read from file", argv[1], fd_from, -1);
-        return (98);
-    }
-
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        print_error("Error: Can't write to", argv[2], fd_from, fd_to);
-        return (99);
-    }
-
-    do {
-        read_count = read(fd_from, buffer, BUFFER_SIZE);
-        if (read_count == -1)
-        {
-            print_error("Error: Can't read from file", argv[1], fd_from, fd_to);
-            return (98);
-        }
-
-        write_count = write(fd_to, buffer, read_count);
-        if (write_count == -1)
-        {
-            print_error("Error: Can't write to", argv[2], fd_from, fd_to);
-            return (99);
-        }
-    } while (read_count > 0);
-
-    if (close(fd_from) == -1)
-    {
-        print_error("Error: Can't close fd", argv[1], fd_from, fd_to);
-        return (100);
-    }
-
-    if (close(fd_to) == -1)
-    {
-        print_error("Error: Can't close fd", argv[2], fd_from, fd_to);
-        return (100);
-    }
-
-    return (0);
+	if (close(fd_from) == -1)
+	{
+		print_error("Error: Can't close fd", argv[1], fd_from, fd_to);
+		return (100);
+	}
+	if (close(fd_to) == -1)
+	{
+		print_error("Error: Can't close fd", argv[2], fd_from, fd_to);
+		return (100);
+	}
+	return (0);
 }
 
 /**
@@ -78,10 +72,10 @@ int main(int argc, char **argv)
 */
 void print_error(char *message, char *filename, int fd_from, int fd_to)
 {
-    dprintf(STDERR_FILENO, "%s %s", message, filename);
-    if (fd_from != -1)
-        close(fd_from);
-    if (fd_to != -1)
-        close(fd_to);
-    dprintf(STDERR_FILENO, "\n");
+	dprintf(STDERR_FILENO, "%s %s", message, filename);
+	if (fd_from != -1)
+		close(fd_from);
+	if (fd_to != -1)
+		close(fd_to);
+		dprintf(STDERR_FILENO, "\n");
 }
