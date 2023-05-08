21459ae3704c7a677931722a717886d22a4cbd08
Benard <githubenard16@gmail.com>
2023-05-08 15:54:50 +0300

    add 2-append_text_to_file.c update main.h

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,30 +13,22 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_WRONLY | O_APPEND);
+	FILE *file = fopen(filename, "a");
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (fd == -1)
+
+	if (file == NULL)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		int bytes = 0;
-		ssize_t bytes_w = write(fd, text_content, bytes);
-
-		while (text_content[bytes] != '\0')
-		{
-			bytes++;
-		}
-		if (bytes_w == -1)
-		{
-			return (-1);
-		}
+		fprintf(file, "%s", text_content);
 	}
-	close(fd);
+
+	fclose(file);
 	return (1);
 }
