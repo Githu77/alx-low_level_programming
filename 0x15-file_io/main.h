ac6f321837d9daaa4250a6284a9a6b8330ed707f
Benard <githubenard16@gmail.com>
2023-05-09 16:14:40 +0300

    add 100-elf_header.c update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,8 +17,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char **argv);
 void print_error(char *message, char *filename, int fd_from, int fd_to);
-void print_error(const char* message);
-void print_elf_header(const Elf64_Ehdr* header);
-int main(int argc, char** argv);
+
 
 #endif
