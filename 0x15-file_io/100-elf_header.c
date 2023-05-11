facbae9ce2a260057f2c2d14298ab4235b7f0ac9
Benard <githubenard16@gmail.com>
2023-05-11 16:24:50 +0300

    fix 100-elf_header.c update main.h

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -7,19 +7,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-void scan_elf(unsigned char *e_ident);
-void print_elf_magic(unsigned char *e_ident);
-void print_elf_class(unsigned char *e_ident);
-void print_elf_data(unsigned char *e_ident);
-void print_elf_version(unsigned char *e_ident);
-void print_elf_osabi(unsigned char *e_ident);
-void print_elf_abi(unsigned char *e_ident);
-void print_elf_type(unsigned int e_type, unsigned char *e_ident);
-void print_elf_entry(unsigned long int e_entry, unsigned char *e_ident);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 
-void scan_elf(unsigned char *in)
+
+void check_elf(unsigned char *in)
 {
 	int index;
 
@@ -38,7 +39,7 @@ void scan_elf(unsigned char *in)
 
 
 
-void print_elf_magic(unsigned char *e_ident)
+void print_magic(unsigned char *e_ident)
 {
 	int i;
 
@@ -54,7 +55,7 @@ void print_elf_magic(unsigned char *e_ident)
 }
 
 
-void print_elf_class(unsigned char *e_ident)
+void print_class(unsigned char *e_ident)
 {
 	printf("  Class:                             ");
 
@@ -75,7 +76,7 @@ void print_elf_class(unsigned char *e_ident)
 }
 
 
-void print_elf_data(unsigned char *e_ident)
+void print_data(unsigned char *e_ident)
 {
     const char *data_type = NULL;
     const unsigned char data_val = e_ident[EI_DATA];
@@ -95,7 +96,7 @@ void print_elf_data(unsigned char *e_ident)
 
 
 
-void print_elf_version(unsigned char *e_ident)
+void print_version(unsigned char *e_ident)
 {
     printf("  Version:                           %d", e_ident[EI_VERSION]);
 
@@ -107,7 +108,7 @@ void print_elf_version(unsigned char *e_ident)
 
 
 
-void print_elf_osabi(unsigned char *e_ident)
+void print_osabi(unsigned char *e_ident)
 {
      printf("  OS/ABI:                            ");
 
@@ -138,7 +139,7 @@ void print_elf_osabi(unsigned char *e_ident)
 
 
 
-void print_elf_abi(unsigned char *e_ident)
+void print_abi(unsigned char *e_ident)
 {
 	if (e_ident[EI_ABIVERSION] == 0) {
 		printf("  ABI Version:                       unspecified\n");
@@ -150,7 +151,7 @@ void print_elf_abi(unsigned char *e_ident)
 
 
 
-void print_elf_type(unsigned int e_type, unsigned char *e_ident)
+void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	const char *types[] = {
 		"NONE (None)",
@@ -176,7 +177,7 @@ void print_elf_type(unsigned int e_type, unsigned char *e_ident)
 
 
 
-void print_elf_entry(unsigned long int e_entry, unsigned char *e_ident)
+void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
     printf("  Entry point address:               %#lx\n", e_entry);
 
@@ -229,16 +230,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	scan_elf(header->e_ident);
+	check_elf(header->e_ident);
 	printf("ELF Header:\n");
-	print_elf_magic(header->e_ident);
-	print_elf_class(header->e_ident);
-	print_elf_data(header->e_ident);
-	print_elf_version(header->e_ident);
-	print_elf_osabi(header->e_ident);
-	print_elf_abi(header->e_ident);
-	print_elf_type(header->e_type, header->e_ident);
-	print_elf_entry(header->e_entry, header->e_ident);
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
 	close_elf(o);
