d7de8aa335b45bc3580ff15e3b7a008a593e63ec
Benard <githubenard16@gmail.com>
2023-05-11 18:45:13 +0300

    fix 100-elf_header.c update main.h

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -155,49 +155,40 @@ void print_elf_osabi(unsigned char *e_ident)
 {
 	 printf("  OS/ABI:                            ");
 
-	if (e_ident[EI_OSABI] == ELFOSABI_NONE)
+	switch (e_ident[EI_OSABI])
 	{
-	printf("UNIX - System V\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_HPUX)
-	{
-	printf("UNIX - HP-UX\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_NETBSD)
-	{
-	printf("UNIX - NetBSD\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_LINUX)
-	{
-	printf("UNIX - Linux\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_SOLARIS)
-	{
-	printf("UNIX - Solaris\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_IRIX)
-	{
-	printf("UNIX - IRIX\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_FREEBSD)
-	{
-	printf("UNIX - FreeBSD\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_TRU64)
-	{
-	printf("UNIX - TRU64\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_ARM)
-	{
-	printf("ARM\n");
-	}
-	else if (e_ident[EI_OSABI] == ELFOSABI_STANDALONE)
-	{
-	printf("Standalone App\n");
-	}
-	else
-	{
-	printf("<unknown: %x>\n", e_ident[EI_OSABI]);
+	case ELFOSABI_NONE:
+		printf("UNIX - System V\n");
+		break;
+	case ELFOSABI_HPUX:
+		printf("UNIX - HP-UX\n");
+		break;
+	case ELFOSABI_NETBSD:
+		printf("UNIX - NetBSD\n");
+		break;
+	case ELFOSABI_LINUX:
+		printf("UNIX - Linux\n");
+		break;
+	case ELFOSABI_SOLARIS:
+		printf("UNIX - Solaris\n");
+		break;
+	case ELFOSABI_IRIX:
+		printf("UNIX - IRIX\n");
+		break;
+	case ELFOSABI_FREEBSD:
+		printf("UNIX - FreeBSD\n");
+		break;
+	case ELFOSABI_TRU64:
+		printf("UNIX - TRU64\n");
+		break;
+	case ELFOSABI_ARM:
+		printf("ARM\n");
+		break;
+	case ELFOSABI_STANDALONE:
+		printf("Standalone App\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
 }
 
