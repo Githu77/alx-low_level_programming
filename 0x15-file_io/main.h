f2cb735e559bd9f894b6a0332e6f6a3d0ca8e0de
Benard <githubenard16@gmail.com>
2023-05-11 14:01:51 +0300

    fix 100-elf_header.c update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,6 +17,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char **argv);
 void print_error(char *message, char *filename, int fd_from, int fd_to);
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]);
 
 #endif
